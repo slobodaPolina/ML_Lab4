11811aa00d191b840a0d8eedcda3af51282fe9ec
PSloboda <PSloboda@ibs.ru>
2020-03-19 18:16:40 +0300

    отчет, исправления

--- naive_bayes.py
+++ naive_bayes.py
@@ -5,124 +5,89 @@ from sklearn.metrics import roc_curve, accuracy_score
 import matplotlib.pyplot as plt
 import numpy as np
 
-dataset = prepare_data()
 
-vectorizer = CountVectorizer()
+def mean_accuracy_plotter(dataset):
+    # полагаем изначально долю спама в сообщениях 50% и она падает до MIN_SPAM_PROPORTION (априорная вероятность встретить спам), в зависимости от этого получаем accuracy
+    spam_proportion = 0.5
+    accuracies = []
+    spam_proportions = []
+    while spam_proportion >= MIN_SPAM_PROPORTION:
+        # собираем средние значения точности классификации для данной априорной вероятности спама
+        accuracies.append(mean_accuracy(dataset, spam_proportion))
+        spam_proportions.append(spam_proportion)
+        spam_proportion -= 0.05
+    plt.plot(spam_proportions, accuracies)
+    print(accuracies, spam_proportions)
+    plt.ylabel('Accuracy')
+    plt.xlabel('Spam proportion')
+    plt.show()
 
 
 def mean_accuracy(dataset, spam_proportion=0):
     accuracies = []
-
     for part_number in range(len(dataset)):
+        model = MultinomialNB() if spam_proportion == 0 else MultinomialNB(fit_prior=True, class_prior=[spam_proportion, 1 - spam_proportion])
+        # cross-validation splitting
         train_features, train_labels, test_features, test_labels = split_data(part_number, dataset)
-
+        # vectorizer считает вхождения слов из тренировочных и тестовых писем
         train_word_counts = vectorizer.fit_transform(train_features)
-
-        if spam_proportion == 0:
-            model = MultinomialNB()
-        else:
-            model = MultinomialNB(fit_prior=False, class_prior=[spam_proportion, 1])
-
-        model.fit(train_word_counts, train_labels)
-
         test_word_counts = vectorizer.transform(test_features)
-
+        # обучаем модель и получаем результат с тестовых данных
+        model.fit(train_word_counts, train_labels)
         accuracies.append(accuracy_score(test_labels, model.predict(test_word_counts)))
-
-    print(sum(accuracies)/len(accuracies))
-
     return sum(accuracies)/len(accuracies)
 
 
 def roc(dataset):
-    squashed_dataset = []
+    messages = []
+    for i in dataset:
+        for message in i:
+            messages.append(message)
+    dataset = np.array(messages)
+    features = dataset[:, 0]
+    labels = dataset[:, 1]
 
-    for part in dataset:
-        for message in part:
-            squashed_dataset.append(message)
-
-    squashed_dataset = np.array(squashed_dataset)
-    dataset = squashed_dataset
-
-    train_features = dataset[:, 0]
-    train_labels = dataset[:, 1]
-
-    test_features = train_features
-    test_labels = train_labels
-
-    train_word_counts = vectorizer.fit_transform(train_features)
+    # Learn the vocabulary dictionary and return term-document matrix, This is equivalent to fit followed by transform, but more efficiently implemented.
+    train_word_counts = vectorizer.fit_transform(features)
+    # Transform documents to document-term matrix. Extract token counts out of raw text documents using the vocabulary fitted
+    test_word_counts = vectorizer.transform(features)
 
     model = MultinomialNB()
-    model.fit(train_word_counts, train_labels)
+    model.fit(train_word_counts, labels)
 
-    test_word_counts = vectorizer.transform(test_features)
-
-    false_positive_rate, true_positive_rate, threshold = roc_curve(test_labels, model.predict_proba(test_word_counts)[:, 0], pos_label='0')
-
-    plt.title('ROC')
+    # label of positive class is 0 (spam class)
+    # Return probability estimates for the test vector X
+    false_positive_rate, true_positive_rate, _ = roc_curve(labels, model.predict_proba(test_word_counts)[:, 0], pos_label='0')
     plt.plot(false_positive_rate, true_positive_rate)
-    plt.plot([0, 1], [0, 1], color='lightgrey', linestyle='--')
-    plt.xlim([-0.05, 1.0])
-    plt.ylim([0.0, 1.05])
     plt.ylabel('True Positive Rate')
     plt.xlabel('False Positive Rate')
     plt.show()
 
 
 def zero_false_positives(dataset):
-    squashed_dataset = []
+    messages = []
+    for i in dataset:
+        for message in i:
+            messages.append(message)
+    dataset = np.array(messages)
 
-    for part in dataset:
-        for message in part:
-            squashed_dataset.append(message)
+    features = dataset[:, 0]
+    labels = dataset[:, 1]
 
-    squashed_dataset = np.array(squashed_dataset)
-    dataset = squashed_dataset
-
-    train_features = dataset[:, 0]
-    train_labels = dataset[:, 1]
-
-    test_features = train_features
-    test_labels = train_labels
-
-    train_word_counts = vectorizer.fit_transform(train_features)
-
-    model = MultinomialNB(fit_prior=False, class_prior=[
-        0.0000000000000000000000000000000000000000000000000000000000000000000000000000000000001, 1])
-    model.fit(train_word_counts, train_labels)
-
-    test_word_counts = vectorizer.transform(test_features)
-
-    false_positive_rate, true_positive_rate, threshold = roc_curve(test_labels,
-                                                                   model.predict_proba(test_word_counts)[:, 0],
-                                                                   pos_label='0')
-
-    false_positive_count = sum([1 for actual, predicted in zip(test_labels, model.predict(test_word_counts)) if
-                                (actual != predicted) and (actual == '1')])
-
+    # Learn the vocabulary dictionary and return term-document matrix
+    train_word_counts = vectorizer.fit_transform(features)
+    # Transform documents to document-term matrix
+    test_word_counts = vectorizer.transform(features)
+    model = MultinomialNB(fit_prior=True, class_prior=[MIN_SPAM_PROPORTION, 1 - MIN_SPAM_PROPORTION])
+    model.fit(train_word_counts, labels)
+    false_positive_count = sum([1 for actual, predicted in zip(labels, model.predict(test_word_counts)) if (actual != predicted) and (actual == '1')])
     print(false_positive_count)
 
-
-def mean_accuracy_progression(dataset, min_spam_proportion):
-    spam_proportion = 1
-    accuracies = []
-    spam_proportions = []
-
-    while spam_proportion >= min_spam_proportion:
-        accuracies.append(mean_accuracy(dataset, spam_proportion))
-
-        spam_proportions.append(spam_proportion)
-
-        spam_proportion = spam_proportion / 10000000000
-
-    plt.title('Accuracy')
-    plt.plot(accuracies, spam_proportions)
-    plt.ylabel('Accuracy')
-    plt.xlabel('Spam/Legit')
-    plt.show()
-
-min_spam_proportion = 0.0000000000000000000000000000000000000000000000000000000000000000000000000000000000001
-
-#mean_accuracy_progression(dataset, min_spam_proportion)
-
+MIN_SPAM_PROPORTION = pow(10, -85)
+dataset = prepare_data()
+# Convert a collection of text documents to a matrix of token counts - Считаем частоту вхождения слов с помощью этой модели
+vectorizer = CountVectorizer()
+# used it to guess MIN_SPAM_PROPORTION in order to get '0' output
+#zero_false_positives(dataset)
+#mean_accuracy_plotter(dataset)
 roc(dataset)
--- prepare_data.py
+++ prepare_data.py
@@ -1,58 +1,32 @@
 import os
-import pandas as pd
 import numpy as np
 
 
+# считываем данные из всех файлов в многомерный массив
 def prepare_data():
-    path = './data'
-
     dataset = []
-
-    part_folder_paths = [f.path for f in os.scandir(path) if f.is_dir()]
-
-    for part_path in part_folder_paths:
-        file_paths = [f.path for f in os.scandir(part_path) if f.is_file()]
-
-        part = []
-
-        for file_path in file_paths:
+    subdirectories = [f.path for f in os.scandir('./data') if f.is_dir()]
+    for subdirectory in subdirectories:
+        files = [f.path for f in os.scandir(subdirectory) if f.is_file()]
+        messages = []
+        for file_path in files:
             file = open(file_path)
-
-            if 'legit' in file_path:
-                # legit
-                label = 1
-            else:
-                # spmsg
-                label = 0
-
             features = np.array(file.read())
-
-            message = np.array([features, label])
-
-            part.append(message)
+            # spam messages contain the word 'spmsg' in the title, not spam ones - 'legit'
+            message = np.array([features, 1 if 'legit' in file_path else 0])
+            messages.append(message)
             file.close()
-
-        dataset.append(part)
-
-    dataset = np.array(dataset)
-
-    return dataset
+        dataset.append(messages)
+    return np.array(dataset)
 
 
+# разделяем данные - что под номером test_part_num - будет тестовое, все остальные данные - трейновые
 def split_data(test_part_num, dataset):
-    test_features = dataset[test_part_num][:, 0]
-    test_labels = dataset[test_part_num][:, 1]
-
     train_messages = []
-
     for part_number in range(len(dataset)):
         if part_number != test_part_num:
             for message in dataset[part_number]:
                 train_messages.append(message)
-
     train_messages = np.array(train_messages)
-
-    train_features = train_messages[:, 0]
-    train_labels = train_messages[:, 1]
-
-    return train_features, train_labels, test_features, test_labels
+    # train features, labels, test features, labels
+    return train_messages[:, 0], train_messages[:, 1], dataset[test_part_num][:, 0], dataset[test_part_num][:, 1]
